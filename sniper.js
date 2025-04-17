e6d41a831c85aee380598eea997bca236e6cd717
fordaaaa <foworda@gmail.com>
2025-04-17 14:52:47 -0400

    Update sniper.js

--- sniper.js
+++ sniper.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
 
-const usernameToSnag = 'desired_username'; // replace with username **
-const checkInterval = 1000; // change checking delay
+const targetuser = 'desired_username'; // replace with username **
+const checkdelay = 1000; // change checking delay
 
 async function checkUsernameAvailability(username) {
     try {
         const response = await axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`);
-        return response.status === 204; // 204 no content = avalible username
+        return response.status === 204; // 204 no content = available username
     } catch (error) {
         if (error.response && error.response.status === 204) {
             return true; 
@@ -14,3 +14,10 @@ async function checkUsernameAvailability(username) {
         return false; 
     }
 }
+
+async function snipinguser(username) {
+    // implement logic
+    console.log(`Scoping user: ${username}`);
+    // auth + send request for username
+}
+
